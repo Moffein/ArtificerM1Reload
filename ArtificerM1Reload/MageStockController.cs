6d2af6ecc79eed4d572c9f8acb28bbe695e327c9
Moffein <22742925+Moffein@users.noreply.github.com>
2024-05-27 01:38:44 -0700

    Now supports reload time/stocks to recharge.

--- ArtificerM1Reload/MageStockController.cs
+++ ArtificerM1Reload/MageStockController.cs
@@ -9,7 +9,7 @@ namespace ArtificerM1Reload
     public class MageStockController : MonoBehaviour
     {
         public static float graceDuration = 0.4f;    //Used when there's still stocks in the mag
-        public static float baseDuration = 1f;
+        public static float defaultReloadTime = 1f;
 
         private CharacterBody body;
         private SkillLocator skills;
@@ -23,7 +23,7 @@ namespace ArtificerM1Reload
         public static GameObject lightningMuzzleflashEffectPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mage/MuzzleflashMageLightning.prefab").WaitForCompletion();
         public static GameObject iceMuzzleflashEffectPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Junk/Mage/MuzzleflashMageIce.prefab").WaitForCompletion();
 
-        public static Dictionary<Type, GameObject> StatePairs = new Dictionary<Type, GameObject>();
+        public static Dictionary<Type, SkillInfo> StatePairs = new Dictionary<Type, SkillInfo>();
 
         private void Awake()
         {
@@ -50,9 +50,11 @@ namespace ArtificerM1Reload
                     reloadStopwatch -= Time.fixedDeltaTime;
                     if (reloadStopwatch <= 0f)
                     {
-                        reloadStopwatch += baseDuration / body.attackSpeed;
+                        reloadStopwatch += GetReloadDuration() / body.attackSpeed;
 
-                        skills.primary.AddOneStock();
+                        skills.primary.stock += GetRechargeStock();
+                        skills.primary.rechargeStopwatch = 0f;
+
                         Util.PlaySound("Play_railgunner_m2_reload_basic", base.gameObject);
                         ShowReloadVFX();
 
@@ -65,26 +67,78 @@ namespace ArtificerM1Reload
             }
             else
             {
-                reloadStopwatch = baseDuration / body.attackSpeed;
+                reloadStopwatch = GetReloadDuration() / body.attackSpeed;
             }
         }
 
         public void FireSkill(float duration)
         {
             delayStopwatch = graceDuration;  //Duration is already scaled to attack speed. InitialDelay is simply for inputs, and is ignored if the mag is empty.
-            reloadStopwatch = baseDuration / body.attackSpeed;// + (skills.primary.stock <= 0 ? duration : 0f);
+            reloadStopwatch = GetReloadDuration() / body.attackSpeed;// + (skills.primary.stock <= 0 ? duration : 0f);
         }
+
         private void ShowReloadVFX()
         {
-            GameObject currentEffectPrefab = null;
-            StatePairs.TryGetValue(skills.primary.activationState.stateType, out currentEffectPrefab);
+            SkillInfo currentSkill = null;
+            StatePairs.TryGetValue(skills.primary.activationState.stateType, out currentSkill);
 
-            if (currentEffectPrefab)
+            if (currentSkill != null && currentSkill.reloadEffectPrefab != null)
             {
-                EffectManager.SimpleMuzzleFlash(currentEffectPrefab, base.gameObject, rightMuzzle ? "MuzzleRight" : "MuzzleLeft", true);
+                EffectManager.SimpleMuzzleFlash(currentSkill.reloadEffectPrefab, base.gameObject, rightMuzzle ? "MuzzleRight" : "MuzzleLeft", true);
             }
 
             rightMuzzle = !rightMuzzle;
+        }
+
+        private float GetReloadDuration()
+        {
+            SkillInfo currentSkill = null;
+            StatePairs.TryGetValue(skills.primary.activationState.stateType, out currentSkill);
+
+            if (currentSkill != null)
+            {
+                return currentSkill.baseReloadDuration;
+            }
+            else
+            {
+                return defaultReloadTime;
+            }
+        }
+
+        private int GetRechargeStock()
+        {
+            SkillInfo currentSkill = null;
+            StatePairs.TryGetValue(skills.primary.activationState.stateType, out currentSkill);
+
+            if (currentSkill != null)
+            {
+                return currentSkill.rechargeStock;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public class SkillInfo
+        {
+            public GameObject reloadEffectPrefab;
+            public float baseReloadDuration;
+            public int rechargeStock;
+
+            public SkillInfo()
+            {
+                reloadEffectPrefab = fireMuzzleflashEffectPrefab;
+                baseReloadDuration = defaultReloadTime;
+                rechargeStock = 1;
+            }
+
+            public SkillInfo(GameObject reloadEffectPrefab, float baseCooldown, int rechargeStock)
+            {
+                this.reloadEffectPrefab = reloadEffectPrefab;
+                this.baseReloadDuration = baseCooldown;
+                this.rechargeStock = rechargeStock;
+            }
         }
     }
 }
--- ArtificerM1Reload/Plugin.cs
+++ ArtificerM1Reload/Plugin.cs
@@ -20,20 +20,24 @@ namespace R2API.Utils
 
 namespace ArtificerM1Reload
 {
-    [BepInPlugin("com.Moffein.ArtificerM1Reload", "ArtificerM1Reload", "1.0.1")]
+    [BepInPlugin("com.Moffein.ArtificerM1Reload", "ArtificerM1Reload", "1.1.0")]
     public class ArtificerM1Reload : BaseUnityPlugin
     {
+
         public void Awake()
         {
             MageStockController.graceDuration = base.Config.Bind<float>(new ConfigDefinition("Stats", "Grace Duration"), 0.4f, new ConfigDescription("Time after firing before the reload starts. Ignored when out of stocks.")).Value;
-            MageStockController.baseDuration = base.Config.Bind<float>(new ConfigDefinition("Stats", "Reload Time"), 1f, new ConfigDescription("Time it takes to reload a shot.")).Value;
+            MageStockController.defaultReloadTime = base.Config.Bind<float>(new ConfigDefinition("Stats", "Reload Time"), 1f, new ConfigDescription("Time it takes to reload a shot. Modded skills may have a different value from this.")).Value;
 
             GameObject bodyPrefab = LegacyResourcesAPI.Load<GameObject>("Prefabs/CharacterBodies/MageBody");
             SkillDef fireBoltDef = Addressables.LoadAssetAsync<SkillDef>("RoR2/Base/Mage/MageBodyFireFirebolt.asset").WaitForCompletion();
             SkillDef plasmaBoltDef = Addressables.LoadAssetAsync<SkillDef>("RoR2/Base/Mage/MageBodyFireLightningBolt.asset").WaitForCompletion();
 
-            MageStockController.StatePairs.Add(typeof(EntityStates.Mage.Weapon.FireFireBolt), MageStockController.fireMuzzleflashEffectPrefab);
-            MageStockController.StatePairs.Add(typeof(EntityStates.Mage.Weapon.FireLightningBolt), MageStockController.lightningMuzzleflashEffectPrefab);
+            MageStockController.SkillInfo fireBoltInfo = new MageStockController.SkillInfo(MageStockController.fireMuzzleflashEffectPrefab, MageStockController.defaultReloadTime, 1);
+            MageStockController.SkillInfo plasmaBoltInfo = new MageStockController.SkillInfo(MageStockController.lightningMuzzleflashEffectPrefab, MageStockController.defaultReloadTime, 1);
+
+            MageStockController.StatePairs.Add(typeof(EntityStates.Mage.Weapon.FireFireBolt), fireBoltInfo);
+            MageStockController.StatePairs.Add(typeof(EntityStates.Mage.Weapon.FireLightningBolt), plasmaBoltInfo);
             bodyPrefab.AddComponent<MageStockController>();
 
             fireBoltDef.rechargeStock = 0;
